d2e6f539aafcda01d0fc69231dc8b1c8ba7215ad
SamuelNayo <nsamuel@space.gov.rw>
2022-10-11 14:05:12 +0200

    modification

--- src/reports/models.py
+++ src/reports/models.py
@@ -5,7 +5,7 @@ from profiles.models import Profile
 class Report(models.Model):
   name = models.CharField(max_length=120)
   image = models.ImageField(upload_to='reports', blank=True)
-  remarrks = models.TextField()
+  remarks = models.TextField()
   author = models.ForeignKey(Profile, on_delete=models.CASCADE)
   created = models.DateField(auto_now_add=True)
   updated = models.DateTimeField(auto_now=True)
